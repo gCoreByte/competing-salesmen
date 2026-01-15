16003a686bdccb65e7b8e8baf32b364e06302538
CoreByte <21132799+gCoreByte@users.noreply.github.com>
2026-01-15 20:55:59 +0200

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header class="p-4 mb-4">
-    <nav class="navbar px-4 border rounded border-neutral">
-      <RouterLink class="btn btn-ghost text-xl" to="/">Home</RouterLink>
-      <RouterLink class="btn btn-ghost text-xl" to="/about">About</RouterLink>
-    </nav>
+    <nav class="navbar px-4"></nav>
   </header>
 
   <RouterView class="p-4" />
